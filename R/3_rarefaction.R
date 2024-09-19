06499bb23e3be8a4e1f7cc0a001185ca4d9cb9ec
Bethany Allen <bethany.allen@bsse.ethz.ch>
2024-09-19 16:20:13 +0200

    Finish rarefaction code

--- R/3_rarefaction.R
+++ R/3_rarefaction.R
@@ -26,17 +26,22 @@ collection_counts <- fossils %>%
 to_sample <- min(collection_counts$n)
 
 #Create data frame to store sampled richness values, starting with stages
-subsampled_richness <- data.frame(stages)
+subsampled_richness <- data.frame()
 
 #Sample that number of collections for each stage, and count species
-for (i in 1:100) {
+for (j in 1:length(stages)) {
   stage_filter <- fossils %>% filter(identified_rank == "species") %>%
-                  filter(stage_bin == "Asselian")
-  unique_collections <- unique(stage_filter$collection_no_pooled)
-  coll_sample <- sample(unique_collections, size = to_sample,
-                        replace = FALSE)
-  sampled_collections <- filter(stage_filter,
-                                collection_no_pooled %in% coll_sample)
-  species_count <- length(unique(sampled_collections$accepted_name))
-  subsampled_richness[i,2] <- species_count
+                    filter(stage_bin == stages[j])
+  for (i in 1:100) {
+    unique_collections <- unique(stage_filter$collection_no_pooled)
+    coll_sample <- sample(unique_collections, size = to_sample,
+                          replace = FALSE)
+    sampled_collections <- filter(stage_filter,
+                                  collection_no_pooled %in% coll_sample)
+    species_count <- length(unique(sampled_collections$accepted_name))
+    subsampled_richness[i,j] <- species_count
+  }
 }
+
+#Label stages
+colnames(subsampled_richness) <- stages
