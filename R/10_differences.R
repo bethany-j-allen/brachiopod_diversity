441eff3a78cdd192c0f0597605245fd71a78b4b9
Bethany Allen <ballen@gfz.de>
2025-04-28 22:10:03 +0200

    Round differences

--- R/10_differences.R
+++ R/10_differences.R
@@ -23,34 +23,34 @@ results_table <- select(counts, stage, mid_ma, level)
 
 for (i in 1:(length(genus_rows) - 1)){
   #Raw
-  results_table[(i*2-1),4] <- ((counts$raw[genus_rows[i+1]] -
-                           counts$raw[genus_rows[i]])/counts$raw[genus_rows[i]]) * 100
-  results_table[(i*2),4] <- ((counts$raw[species_rows[i+1]] -
-                           counts$raw[species_rows[i]])/counts$raw[species_rows[i]]) * 100
+  results_table[(i*2-1),4] <- round(((counts$raw[genus_rows[i+1]] -
+                           counts$raw[genus_rows[i]])/counts$raw[genus_rows[i]]) * 100, digits = 2)
+  results_table[(i*2),4] <- round(((counts$raw[species_rows[i+1]] -
+                           counts$raw[species_rows[i]])/counts$raw[species_rows[i]]) * 100, digits = 2)
 
   #Range-through
-  results_table[(i*2-1),5] <- ((counts$rt[genus_rows[i+1]] -
-                                  counts$rt[genus_rows[i]])/counts$rt[genus_rows[i]]) * 100
-  results_table[(i*2),5] <- ((counts$rt[species_rows[i+1]] -
-                                counts$rt[species_rows[i]])/counts$rt[species_rows[i]]) * 100
+  results_table[(i*2-1),5] <- round(((counts$rt[genus_rows[i+1]] -
+                                  counts$rt[genus_rows[i]])/counts$rt[genus_rows[i]]) * 100, digits = 2)
+  results_table[(i*2),5] <- round(((counts$rt[species_rows[i+1]] -
+                                counts$rt[species_rows[i]])/counts$rt[species_rows[i]]) * 100, digits = 2)
 
   #Rarefaction
-  results_table[(i*2-1),6] <- ((counts$rf_median[genus_rows[i+1]] -
-                                  counts$rf_median[genus_rows[i]])/counts$rf_median[genus_rows[i]]) * 100
-  results_table[(i*2),6] <- ((counts$rf_median[species_rows[i+1]] -
-                                counts$rf_median[species_rows[i]])/counts$rf_median[species_rows[i]]) * 100
+  results_table[(i*2-1),6] <- round(((counts$rf_median[genus_rows[i+1]] -
+                                  counts$rf_median[genus_rows[i]])/counts$rf_median[genus_rows[i]]) * 100, digits = 2)
+  results_table[(i*2),6] <- round(((counts$rf_median[species_rows[i+1]] -
+                                counts$rf_median[species_rows[i]])/counts$rf_median[species_rows[i]]) * 100, digits = 2)
 
   #Squares
-  results_table[(i*2-1),7] <- ((counts$squares[genus_rows[i+1]] -
-                                  counts$squares[genus_rows[i]])/counts$squares[genus_rows[i]]) * 100
-  results_table[(i*2),7] <- ((counts$squares[species_rows[i+1]] -
-                                counts$squares[species_rows[i]])/counts$squares[species_rows[i]]) * 100
+  results_table[(i*2-1),7] <- round(((counts$squares[genus_rows[i+1]] -
+                                  counts$squares[genus_rows[i]])/counts$squares[genus_rows[i]]) * 100, digits = 2)
+  results_table[(i*2),7] <- round(((counts$squares[species_rows[i+1]] -
+                                counts$squares[species_rows[i]])/counts$squares[species_rows[i]]) * 100, digits = 2)
 
   #SQS
-  results_table[(i*2-1),8] <- ((counts$qD[genus_rows[i+1]] -
-                                  counts$qD[genus_rows[i]])/counts$qD[genus_rows[i]]) * 100
-  results_table[(i*2),8] <- ((counts$qD[species_rows[i+1]] -
-                                counts$qD[species_rows[i]])/counts$qD[species_rows[i]]) * 100
+  results_table[(i*2-1),8] <- round(((counts$qD[genus_rows[i+1]] -
+                                  counts$qD[genus_rows[i]])/counts$qD[genus_rows[i]]) * 100, digits = 2)
+  results_table[(i*2),8] <- round(((counts$qD[species_rows[i+1]] -
+                                counts$qD[species_rows[i]])/counts$qD[species_rows[i]]) * 100, digits = 2)
 }
 
 #Rename columns
