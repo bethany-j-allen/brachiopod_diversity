09405b4d671eb4240fd8cb0ad1ca6c4c8150cdc7
Bethany Allen <ballen@gfz.de>
2025-10-08 15:19:22 +0200

    Add provisional geographic regions

--- R/11_palaeomap.R
+++ R/11_palaeomap.R
@@ -6,6 +6,12 @@
 #Load packages
 library(palaeoverse)
 library(tidyverse)
+library(viridis)
+
+#Create a vector giving the chronological order of stages
+stages <- c("Asselian", "Sakmarian", "Artinskian", "Kungurian", "Roadian",
+            "Wordian", "Capitanian", "Wuchiapingian", "Changhsingian", "Induan",
+            "Olenekian", "Anisian", "Ladinian")
 
 #Read in dataset
 fossils <- read_csv("data/brachiopods_clean.csv")
@@ -28,8 +34,60 @@ fossils <- palaeorotate(occdf = as.data.frame(fossils),
 #Remove points which cannot be rotated
 fossils <- filter(fossils, !is.na(p_lat))
 
+#Add column to hold geographic realm information
+fossils$realm <- NA
+#Allocate points to geographic realms
+for (i in (1:nrow(fossils))) {
+  if (fossils$p_lng[i] < -100 && fossils$p_lat[i] < -50) {
+    fossils$realm[i] <- "A"
+  } else if (-100 < fossils$p_lng[i] &
+             fossils$p_lng[i] < -15 &
+             -60 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 60) {
+    fossils$realm[i] <- "B"
+  } else if (fossils$p_lng[i] > 140 &
+             fossils$p_lat[i] < -25) {
+    fossils$realm[i] <- "C"
+  } else if (60 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 120 &
+             -10 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 30) {
+    fossils$realm[i] <- "D"
+  } else if (120 < fossils$p_lng[i] &
+             20 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 60) {
+    fossils$realm[i] <- "D"
+  } else if (0 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 150 &
+             -75 < fossils$p_lat[i] &
+             fossils$p_lat[i] < -25) {
+    fossils$realm[i] <- "E"
+    } else if (55 < fossils$p_lng[i] &
+               fossils$p_lng[i] < 75 &
+               -25 < fossils$p_lat[i] &
+               fossils$p_lat[i] < -20) {
+      fossils$realm[i] <- "E"
+    } else if (0 < fossils$p_lng[i] &
+         fossils$p_lng[i] < 75 &
+         -25 < fossils$p_lat[i] &
+         fossils$p_lat[i] < 30) {
+      fossils$realm[i] <- "F"
+    } else if (20 < fossils$p_lng[i] &
+               fossils$p_lng[i] < 80 &
+               25 < fossils$p_lat[i] &
+               fossils$p_lat[i] < 50) {
+      fossils$realm[i] <- "F"
+    } else if (50 < fossils$p_lng[i] &
+               40 < fossils$p_lat[i]) {
+      fossils$realm[i] <- "F"
+    } else {fossils$realm[i] <- "G"}
+}
+
 #Plot points
-ggplot(fossils, aes(x = p_lng, y = p_lat, group = stage_bin, col = stage_bin)) +
+ggplot(fossils, aes(x = p_lng, y = p_lat, group = realm, col = realm)) +
   geom_point() +
+  scale_colour_viridis(discrete = T)+
   xlim(-180, 180) +
-  ylim(-90, 90)
+  ylim(-90, 90) +
+  theme_classic()
+
