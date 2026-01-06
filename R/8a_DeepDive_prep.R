d3249c9c149229695a749d8f3ed128184f07830b
Bethany Allen <ballen@gfz.de>
2026-01-06 14:40:46 +0100

    Merge palaeogeographic realms into DeepDiveR inputs

--- R/8a_DeepDive_prep.R
+++ R/8a_DeepDive_prep.R
@@ -1,45 +1,103 @@
-#Bethany Allen   1st May 2025
-#Code to generate DeepDive inputs
+#Bethany Allen   6th Jan 2026
+#Code to generate DeepDive inputs, including palaeogeographic partitioning
 
 #setwd("#####")
 
 #Load packages
 library(remotes)
-remotes::install_github("DeepDive-project/DeepDiveR")
+#remotes::install_github("DeepDive-project/DeepDiveR")
 library(DeepDiveR)
-library(countrycode)
+library(palaeoverse)
 library(tidyverse)
+library(viridis)
+
+#Create a vector giving the chronological order of stages
+stages <- c("Asselian", "Sakmarian", "Artinskian", "Kungurian", "Roadian",
+            "Wordian", "Capitanian", "Wuchiapingian", "Changhsingian", "Induan",
+            "Olenekian", "Anisian", "Ladinian")
 
 #Read in dataset
 fossils <- read_csv("data/brachiopods_clean.csv")
 
-# List unique country codes
-sort(unique(fossils$cc))
+##Palaeogeography
+#Create midpoint age key
+time_key <- select(GTS2020, interval_name, mid_ma)
 
-# Take ISO 2 letter country codes and convert to continents
-fossils$continent <- countrycode(fossils$cc, "iso2c", "continent")
+#Generate a column of midpoint ages
+fossils <- left_join(fossils, time_key,
+                     by = join_by("stage_bin" == "interval_name"))
 
-# Label country 'UK' as being in Europe
-fossils$continent[which(fossils$cc == "UK")] <- "Europe"
+#Palaeorotate points
+fossils <- palaeorotate(occdf = as.data.frame(fossils),
+                        age = "mid_ma",
+                        model = "MERDITH2021",
+                        uncertainty = FALSE,
+                        round = 2)
 
-# One NA -> actually "North Atlantic", can also be labelled as Europe
-fossils$continent[which(is.na(fossils$cc))] <- "Europe"
+#Remove points which cannot be rotated
+fossils <- filter(fossils, !is.na(p_lat))
 
-# Relabel "Americas" as "South America"
-fossils$continent[which(fossils$continent == "Americas")] <-
-  "South America"
+#Add column to hold geographic realm information
+fossils$realm <- NA
+#Allocate points to geographic realms (loosely based on literature)
+for (i in (1:nrow(fossils))) {
+  if (fossils$p_lng[i] < -100 && fossils$p_lat[i] < -50) {
+    fossils$realm[i] <- "A"
+  } else if (-100 < fossils$p_lng[i] &
+             fossils$p_lng[i] < -15 &
+             -60 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 60) {
+    fossils$realm[i] <- "B"
+  } else if (fossils$p_lng[i] > 140 &
+             fossils$p_lat[i] < -25) {
+    fossils$realm[i] <- "C"
+  } else if (60 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 120 &
+             -10 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 30) {
+    fossils$realm[i] <- "D"
+  } else if (120 < fossils$p_lng[i] &
+             20 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 60) {
+    fossils$realm[i] <- "D"
+  } else if (0 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 150 &
+             -75 < fossils$p_lat[i] &
+             fossils$p_lat[i] < -25) {
+    fossils$realm[i] <- "E"
+  } else if (55 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 75 &
+             -25 < fossils$p_lat[i] &
+             fossils$p_lat[i] < -20) {
+    fossils$realm[i] <- "E"
+  } else if (0 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 75 &
+             -25 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 30) {
+    fossils$realm[i] <- "F"
+  } else if (20 < fossils$p_lng[i] &
+             fossils$p_lng[i] < 80 &
+             25 < fossils$p_lat[i] &
+             fossils$p_lat[i] < 50) {
+    fossils$realm[i] <- "F"
+  } else if (50 < fossils$p_lng[i] &
+             40 < fossils$p_lat[i]) {
+    fossils$realm[i] <- "F"
+  } else {fossils$realm[i] <- "G"}
+}
 
-# Relabel "CA", "US" and "MX" as "North America"
-fossils$continent[which(fossils$cc == "CA")] <- "North America"
-fossils$continent[which(fossils$cc == "US")] <- "North America"
-fossils$continent[which(fossils$cc == "MX")] <- "North America"
+#Plot localities by their palaeogeographic realms
+ggplot(fossils, aes(x = p_lng, y = p_lat, group = realm, col = realm)) +
+  geom_point() +
+  scale_colour_viridis(discrete = T)+
+  xlim(-180, 180) +
+  ylim(-90, 90) +
+  theme_classic()
 
-# Count number of occurrences from each continent
-count(fossils, continent)
-
+##Create DeepDive input files
 # Create dataframe
 brach_data <- data.frame(Taxon = fossils$accepted_name,
-                         Region = fossils$continent,
+                         Region = fossils$realm,
                          MinAge = fossils$min_ma,
                          MaxAge = fossils$max_ma,
                          Locality = fossils$collection_no)
